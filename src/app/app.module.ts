728abd730b3423252b95535331a886923be7467a
Christopher II Lajom <christopheriilajom@gmail.com>
2021-07-31 22:14:36 +0800

    Added details component in assignment-3 folder.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { FormsModule } from '@angular/forms';
 import { UserComponent } from './assignment-2/user/user.component';
+import { DetailsComponent } from './assignment-3/details/details.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent, UserComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent, UserComponent, DetailsComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
