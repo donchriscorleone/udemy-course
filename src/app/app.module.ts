8d5a047b622459f974c5a5cc90d002246425c265
Christopher II Lajom <christopheriilajom@gmail.com>
2021-07-31 21:22:12 +0800

    Added user component in assignment 2 with some bootstrap class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { FormsModule } from '@angular/forms';
+import { UserComponent } from './assignment-2/user/user.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent, UserComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
