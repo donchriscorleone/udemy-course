4331140a220ee66294be14f43fc146f57c4b10c4
Christopher II Lajom <christopheriilajom@gmail.com>
2021-07-31 20:59:21 +0800

    Added isAllowServer

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent implements OnInit {
+  isAllowServer: boolean = false;
+  serverName: string = '';
+  serverCreationStatus = '';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isAllowServer = true;
+    }, 2000);
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server is been created';
+    this.serverName = '';
+  }
+}
